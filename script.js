da778eca07f0ee94a4bfff1349438211d9218bad
Stieilijh <saitushar02@gmail.com>
2022-04-17 21:51:38 +0530

    Make operator work only if theres a number before

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
+
+window.onload=function(){
+    let operator ="";
+    let first,second;
+    let displayText = document.querySelector("#display");
+    clearDisplay();
+    //Buttons action Listner 
+    const buttons = Array.from(document.querySelectorAll(".buttons"));
+    //numbers
+    const numberbtns = buttons.filter((button)=>{
+    return button.id[0]==="n";
+    });
+    numberbtns.forEach(btn=> {
+        btn.addEventListener("click",()=>{
+            displayText.textContent += btn.id[1];
+    });
+});
+    //operators
+    const operatorbtns = buttons.filter((button)=>{
+        return button.id[0]==="o";
+    });
+    operatorbtns.forEach(btn=> {
+        btn.addEventListener("click",()=>{
+            if(!(isOperator(displayText.textContent.charAt(
+                displayText.textContent.length-1
+            )))){
+                displayText.textContent += btn.id[1];
+            }
+    });
+});
+//checks if given char is operator
+function isOperator(s){
+    return s===""||s==="+"||s==="-"||s==="*"||s==="/";
+}
+
+//clears the screen
+    const clearscr = document.querySelector("#clear");
+    clearscr.addEventListener("click",clearDisplay);
+//backspace one character
+    const backspace = document.querySelector("#back");
+    backspace.addEventListener("click",()=>{
+        displayText.textContent=displayText.textContent.slice(0,-1);
+    });
+
+}
+
+function clearDisplay(){
+    const display =document.querySelector("#display");
+    display.textContent="";
+}
+
 function add(a,b){
     return a+b;
 }
